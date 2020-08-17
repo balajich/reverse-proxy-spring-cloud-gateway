78f67b194e6d877dc83b2735cf37d8a164875836
balajich <balaji.chopparapu@gmail.com>
2020-08-17 10:08:01 +0530

    updated ports

--- restapi/src/main/java/org/eduami/spring/restapi/HelloWorlController.java
+++ restapi/src/main/java/org/eduami/spring/restapi/HelloWorlController.java
@@ -1,12 +1,16 @@
 package org.eduami.spring.restapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorlController {
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping("/**")
     public String helloWorl() {
-        return "Hello World";
+        return "Hello World from: "+serverPort;
     }
 }
